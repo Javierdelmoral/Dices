7f3b4165ca2d454cefa1e5287e08281c6a0b4da3
Javi <51661852+Javierdelmoral@users.noreply.github.com>
2020-04-08 20:49:10 +0200

    first commit

---
+++ Dados/src/test/java/com/it/Dados/DadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.Dados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
